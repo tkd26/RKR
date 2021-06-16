dbe1ae0e97e3111d08b94c7a6ebc1e8c0a013b0c
tkd26 <takedam0226@gmail.com>
2021-06-16 23:28:29 +0900

    normal cifar100 train

---
+++ main.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from model.resnet import resnet18, resnet_addfc
+
+########################################################################
+# Setup
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 64
+epochs = 2
+
+# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+#                                         download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+# testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+#                                        download=True, transform=transform)
+testset = torchvision.datasets.CIFAR100(root='./data', train=False,
+                                       download=True, transform=transform)                                       
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+# classes = ('plane', 'car', 'bird', 'cat',
+#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+classes = (
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 
+    'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 
+    'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 
+    'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse',
+    'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear',
+    'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose',
+    'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake',
+    'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',
+    'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout',
+    'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman',
+    'worm'
+)
+
+########################################################################
+# train
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+# define model
+net = resnet18().to(device)
+net = resnet_addfc(net, 100)
+
+# loss
+criterion = nn.CrossEntropyLoss()
+# optimizer
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# train
+for epoch in range(epochs):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# save trained model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+
+########################################################################
+# eval samples
+
+# dataiter = iter(testloader)
+# images, labels = dataiter.next()
+# images, labels = images.to(device), labels.to(device)
+
+# # print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# # define model
+# net = resnet18().to(device)
+# net = resnet_addfc(net, 100)
+# # load trained model
+# net.load_state_dict(torch.load(PATH))
+
+# outputs = net(images)
+
+# _, predicted = torch.max(outputs, 1)
+
+# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+#                               for j in range(4)))
+
+########################################################################
+# eval score
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testloader:
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        # calculate outputs by running images through the network 
+        outputs = net(inputs)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+########################################################################
+# eval class score
+
+# # prepare to count predictions for each class
+# correct_pred = {classname: 0 for classname in classes}
+# total_pred = {classname: 0 for classname in classes}
+
+# # again no gradients needed
+# with torch.no_grad():
+#     for data in testloader:
+#         images, labels = data
+#         images, labels = images.to(device), labels.to(device)
+#         outputs = net(images)    
+#         _, predictions = torch.max(outputs, 1)
+#         # collect the correct predictions for each class
+#         for label, prediction in zip(labels, predictions):
+#             if label == prediction:
+#                 correct_pred[classes[label]] += 1
+#             total_pred[classes[label]] += 1
+
+  
+# # print accuracy for each class
+# for classname, correct_count in correct_pred.items():
+#     accuracy = 100 * float(correct_count) / total_pred[classname]
+#     print("Accuracy for class {:5s} is: {:.1f} %".format(classname, 
+#                                                    accuracy))
